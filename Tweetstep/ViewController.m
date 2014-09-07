b587f7114ec8291843dad0a62b15ba64eb465106
Canzhi Ye <canzhiye@gmail.com>
2014-09-06 17:05:50 -0700

    socket

--- Tweetstep/ViewController.m
+++ Tweetstep/ViewController.m
@@ -1,14 +1,50 @@
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <SRWebSocketDelegate>
 
 @end
 
 @implementation ViewController
-
+{
+    SRWebSocket *_webSocket;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    _webSocket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ws://localhost:9000/tweetstep"]]];
+    _webSocket.delegate = self;
+    
+    self.title = @"Opening Connection...";
+    [_webSocket open];
+}
+
+#pragma mark - SRWebSocketDelegate
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket;
+{
+    NSLog(@"Websocket Connected");
+    self.title = @"Connected!";
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error;
+{
+    NSLog(@":( Websocket Failed With Error %@", error);
+    
+    self.title = @"Connection Failed! (see logs)";
+    _webSocket = nil;
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message;
+{
+    NSLog(@"Received \"%@\"", message);
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean;
+{
+    NSLog(@"WebSocket closed");
+    self.title = @"Connection Closed! (see logs)";
+    _webSocket = nil;
 }
 
 @end
